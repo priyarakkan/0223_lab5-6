3347c02d77ba2e423a1e2e79eb7965686b9ddf1d
priyarakkan <124619499+priyarakkan@users.noreply.github.com>
2023-03-01 16:19:08 -0800

    Add files via upload

---
+++ 0223_lab5/js/script5.js
@@ -0,0 +1,25 @@
+// VARIABLES
+let miles, gallons, mpg;
+let again = 'y';
+
+do {
+    miles = parseFloat(prompt('Enter the number of miles driven :'));
+    gallons = parseFloat(prompt('Enter the gallon capacity of your vehicle :'));
+    if(!isNaN(miles) && miles > 0 && !isNaN(gallons) && gallons > 0) {
+        mpg = miles / gallons;
+        console.log(`You vehicles gets ${mpg.toFixed(2)} miles per gallon.`)
+    } else {
+        alert('One or both of your entries are invalid.')
+    }
+    
+    //  If it’s anything other than “y” or “n”,
+    // the prompt should 
+    //simply reappear until the user enters a valid entry
+    again = window.prompt('Repeat entries? (y or n)', 'y');
+    if(again == 'y' || again == 'n') {
+        break;
+    } else {
+        alert('Enter valid entry.');
+    }
+} while(again === 'y')
+console.log('Application has ended.')
---
+++ 0223_lab5/js/script6.js
@@ -0,0 +1,31 @@
+// GLOBAL VARIABLES
+let futureValue, investment, rate, years;
+
+// COLLECT THE VALUES FROM THE USER
+//Within investment, the number should be numeric. 
+//Within rate, the number should be numeric and between 0 and a realistic rate like 6%. 
+//For years, the number should be numeric and between 1 and 30.
+
+investment = parseFloat(prompt('Enter investment amount as xxx.xx + <br>'));
+
+rate = parseFloat(prompt('Enter the rate that your hoping to get on your investment as xx.x'));
+
+years = parseInt(prompt(`How many years do you want to invest for?  <br>`));
+
+// CALCULATE THE FUTURE VALUE
+futureValue = investment
+for (let  i = 0; i < years; i++) {
+    if(rate <= 7 && rate >= 0 && years <= 31 && years >= 0 && !isNaN(investment)) {
+        futureValue = futureValue + (futureValue * rate / 100); 
+        document.write(`Investment amount: $${investment.toFixed(2)}<br>`);
+        document.write(`Interest rate: ${rate.toFixed(2)}%<br>`);
+        document.write(`Years: ${years}<br>`);
+        document.write(`Future value: $${futureValue.toFixed(2)}`);
+        break;
+    } else {
+        alert('Enter valid input.') 
+        break;
+    }
+    
+}
+
